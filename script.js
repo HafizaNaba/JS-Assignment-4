8183c2e6adff4f620f191683b10ce8a90cd1a023
Hafiza Naba <naba43152@gmail.com>
2026-05-14 12:16:33 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.write("<h2>" + "Rules for naming JS variables" + "</h2>" +"<br>");
+
+var name1 =("numbers,");
+var name2 =("$");
+var name3 =("_.");
+var name4 =("$my_1stVariable");
+var name5 =("Letter, ");
+var name6 =("$name, _name or name ");
+var name7 =("Sensitive")
+var name8 =("Keywords")
+document.write("Variable names can only contain ,"+ " " + name1 + " " +name2 + " " + "and" + name3 + " "+ "For example : " +name4 +"<br>"  );
+document.write("Variable must begin with a " + name5 + name2 +" "+ "or " + name3  + "For example : " + name6 + "<br>");
+document.write("Variable name are case " + name7 + "<br>")
+document.write("Variable names should not be JS " + name8 )
